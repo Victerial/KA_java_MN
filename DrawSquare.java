3aa8a60d15582a93b0eca7032d9bb22749f86d5b
Nándor <73957841+Victerial@users.noreply.github.com>
2023-09-19 21:25:11 +0200

    Add files via upload

---
+++ DrawSquare.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DrawSquare {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter the square size: ");
+        int size = scan.nextInt();
+
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        scan.close();
+    }
+}
